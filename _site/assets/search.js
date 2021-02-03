99a39648a9275e1fe029c088019ba3ca8809d97a
fengshiwest <2201945951@qq.com>
2021-02-03 20:43:35 +0800

    get start

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Get Start",'url':"/2021/02/03/get-start.html"}]};
